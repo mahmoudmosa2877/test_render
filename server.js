320bfe6c3cfe36865f6aad11b69f3cc0ef9c9ef9
MahmoudMosa77 <114168304+mahmoudmosa2877@users.noreply.github.com>
2024-12-04 18:32:08 +0200

    hhhhhhhhhg

--- server.js
+++ server.js
@@ -7,19 +7,14 @@ app.use(bodyParser.json());
 // Example API key stored in .env file
 const API_KEY = "123456789";
 
-// Middleware to authenticate the API key
-app.use((req, res, next) => {
+// Example endpoint
+app.get('/ping', (req, res) => {
   const userApiKey = req.header('X-API-Key');
   if (userApiKey && userApiKey === API_KEY) {
-    next();
+    res.json({ message: 'API is working!' });
   } else {
     res.status(401).json({ error: 'Unauthorized: Invalid API Key' });
   }
-});
-
-// Example endpoint
-app.get('/ping', (req, res) => {
-  res.json({ message: 'API is working!' });
 });
 const subscriptions = {};
 
